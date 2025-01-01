7eb5ddbfd0c643b8051a2476361045a8dacd26d3
Alexandr Lelikov <alexandr.s.lelikov@gmail.com>
2025-01-01 23:14:15 +0100

    Add russian language to slugify

--- packages/features/ee/organizations/lib/orgDomains.ts
+++ packages/features/ee/organizations/lib/orgDomains.ts
@@ -39,6 +39,7 @@ export function getOrgSlug(hostname: string, forcedSlug?: string) {
   });
 
   if (!currentHostname) {
+    log.warn("Match of WEBAPP_URL with ALLOWED_HOSTNAME failed", { WEBAPP_URL, ALLOWED_HOSTNAMES, hostname });
     return null;
   }
   // Define which is the current domain/subdomain

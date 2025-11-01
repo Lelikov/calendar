d1214a7dfa0624ae1849bd9c0f6286dc6ce9cebd
Alexandr Lelikov <a.lelikov@emergingtravel.com>
2025-11-01 21:10:30 +0100

    Remove unused constants and redundant footer from VerifyEmailByCode template.

--- packages/emails/src/templates/VerifyEmailByCode.tsx
+++ packages/emails/src/templates/VerifyEmailByCode.tsx
@@ -1,4 +1,4 @@
-import { APP_NAME, SENDER_NAME, SUPPORT_MAIL_ADDRESS } from "@calcom/lib/constants";
+import { APP_NAME } from "@calcom/lib/constants";
 
 import type { EmailVerifyCode } from "../../templates/attendee-verify-email";
 import { BaseEmailHtml } from "../components";
@@ -27,20 +27,6 @@ export const VerifyEmailByCode = (
           <>{props.language("verify_email_by_code_email_body")}</>
           <br />
           <p>{props.verificationEmailCode}</p>
-        </p>
-      </div>
-      <div style={{ lineHeight: "6px" }}>
-        <p style={{ fontWeight: 400, lineHeight: "24px" }}>
-          <>
-            {props.language("happy_scheduling")}, <br />
-            <a
-              href={`mailto:${SUPPORT_MAIL_ADDRESS}`}
-              style={{ color: "#3E3E3E" }}
-              target="_blank"
-              rel="noreferrer">
-              <>{props.language("the_calcom_team", { companyName: SENDER_NAME })}</>
-            </a>
-          </>
         </p>
       </div>
     </BaseEmailHtml>
